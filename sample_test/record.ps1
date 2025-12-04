8a8fc452cff36fa0dc490695d48bb682c798f1da
yoyo1575 <2573177898@qq.com>
2025-12-04 23:16:54 +0800

    Delete sample_test directory

--- sample_test/record.ps1
+++
@@ -1,7 +0,0 @@
-
-# 抽样合约solc检查语法
-python sample_jsonl.py
-    "D:\paper\Synthtic_edit_sequence\lintseq\solhint\raw_sol\merge_clean_sol.jsonl" 3
-    "D:\paper\Synthtic_edit_sequence\lintseq\solhint\raw_sol\clean_sol_0.jsonl" 3
-
-python sample_jsonl.py     "D:\paper\Synthtic_edit_sequence\lintseq\solhint\raw_sol\merge_clean_sol.jsonl" 2
